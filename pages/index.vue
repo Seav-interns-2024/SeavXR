d134c5d052ef801c222b6560b4c3ec868438a842
SP-RVN <arvinamandy33@gmail.com>
2024-06-07 13:49:10 +0800

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app>
+      <v-main>
+        <div class="min-h-screen flex flex-col items-center justify-center">
+          <h1 class="text-4xl font-bold text-blue-600">
+            Welcome to Seav XR!
+          </h1>
+          <v-btn color="primary" class="mt-4">
+            Login
+          </v-btn>
+        </div>
+      </v-main>
+    </v-app>
+</template>
---
+++ pages/superAdmin.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app>
+      <v-main>
+        <div class="min-h-screen flex flex-col items-center justify-center">
+          <h1 class="text-4xl font-bold text-blue-600">
+            Welcome to Nuxt with Tailwind CSS and Vuetify!
+          </h1>
+          <v-btn color="primary" class="mt-4">
+            Vuetify Button
+          </v-btn>
+        </div>
+      </v-main>
+    </v-app>
+  </template>
